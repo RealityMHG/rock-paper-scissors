e6dd5008c5a8567c636c9bcdbbc89bb2f1ca3e29
RealityMHG <rafaelrego2000@gmail.com>
2024-06-07 01:03:59 +0100

    Finished the game structure, Game works in 5 rounds

--- functions.js
+++ functions.js
@@ -4,22 +4,95 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-//Single Round of the Game
-function playRound() {
+//Start the Game
+function playGame () {
 
+    //Resetting Scores
+    humanScore = 0;
+    computerScore = 0;
+
+    for (let i = 1; i<=5; i++) {
+
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+
+        console.log("The scores are: " + humanScore + " : " + computerScore);
+
+    }
+
+    if(humanScore > computerScore) console.log("YOU WIN!") 
+        else console.log("Oof better luck next time :/");
+
+}
+
+//Single Round of the Game
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice == "rock") {
+
+        if(computerChoice == "rock")
+            console.log("It's a Tie!, Rock with Rock");
+        
+        if(computerChoice == "paper") {
+            computerScore++;
+            console.log("You Lose!, Paper beats Rock");
+        }
+
+        if(computerChoice == "scissors") {
+            humanScore++;
+            console.log("You Win!, Rock beats Scissors");
+        }
+
+    }
+
+    if(humanChoice == "paper") {
+        
+        if(computerChoice == "rock") {
+            humanScore++;
+            console.log("You Win!, Paper beats Rock");
+        }
+
+        if(computerChoice == "paper")
+            console.log("It's a Tie!, Paper with Paper");
+
+        
+        if(computerChoice == "scissors") {
+            computerScore++;
+            console.log("You Lose!, Scissors beats Paper");
+        }
+
+    }
+
+    if(humanChoice == "scissors") {
+        
+        if(computerChoice == "rock") {
+            computerScore++;
+            console.log("You Lose!, Rock beats Scissors");
+        }
+
+        if(computerChoice == "paper") {
+            humanScore++;
+            console.log("You Win!, Scissors beats Paper");
+        }
+
+        if(computerChoice == "scissors")
+            console.log("It's a Tie!, Scissors with Scissors");
+
+    }
 }
 
 //Get the Computer Choice
 function getComputerChoice() {
     switch (getRandomInt(3)) {
         case 0:
-            return "Rock";
+            return "rock";
 
         case 1:
-            return "Paper";
+            return "paper";
 
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
